d424760d52b39458240efcda8b4523136507fce3
Romulo <rgcostabox@gmail.com>
2019-05-04 20:15:06 -0500

    Homework_01 - isWeekend()

--- homework_01/lecture01.js
+++ homework_01/lecture01.js
@@ -76,8 +76,19 @@ console.log("End of the exercise!!");
 function isWeekend() {
     const todayDate = new Date();
     const day = todayDate.getDate();
-}
 
+    let weekday = [];
+    weekday[0] = 'Sunday';
+    weekday[1] = 'Monnday';
+    weekday[2] = 'Tuesday';
+    weekday[3] = 'Wednesday';
+    weekday[4] = 'Thursday';
+    weekday[5] = 'Friday';
+    weekday[6] = 'Saturday';
+
+    return weekday[day];
+}
+console.log('Today is ' + isWeekend());
 
 //==================================================
 //Exercise 3: applyCoupon()
