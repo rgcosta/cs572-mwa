7c88df13b99cf55cb097379c99dc7a8940541c77
rgcosta <rgcostabox@gmail.com>
2019-05-07 14:47:47 -0500

    homework07 - librarian model

---
+++ homework_07/exercise1.js
@@ -0,0 +1,27 @@
+/*
+BOOKS:
+{ isbn: '',
+  title: '',
+  pages: '',
+  author: [ {firstName: '',
+            lastName: ''
+            } ],
+  tags: [ '', '', ''],
+  copies: '',
+  isAvailable: true
+}
+
+Borrowing:
+{ bw_date: '',
+  return_date: '',
+  books: [{ isbn: '',
+            title: '' }],
+  student: { id: '',
+             name: '',
+             contact: { phone: '',
+                        phone2: '',
+                        address: '' }
+           }
+}
+
+ */
