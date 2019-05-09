a2f84ebf2cdf042d1929f7df3038d6021c25a3ed
Romulo <rgcostabox@gmail.com>
2019-05-08 22:21:33 -0500

    Homework_08 - Exercise, MongoDB queries

--- homework_08/query.js
+++ homework_08/query.js
@@ -19,19 +19,70 @@ client.connect(function (err) {
     // });
 
     //Question 2:
-    // restaurants.find({}).project({'restaurant_id': 1, 'name': 1, 'district': 1, 'cuisine': 1}).toArray(function (err, docs) {
+    // restaurants.find({}).project({"restaurant_id": 1, "name": 1, "district": 1, "cuisine": 1}).toArray(function (err, docs) {
     //     console.dir(docs);
     // });
 
     //Question 3:
-    // restaurants.find({}).project({'_id': 0, 'restaurant_id': 1, 'name': 1, 'district': 1, 'cuisine': 1}).toArray(function (err, docs) {
+    // restaurants.find({}).project({"_id": 0, "restaurant_id": 1, "name": 1, "district": 1, "cuisine": 1}).toArray(function (err, docs) {
     //     console.dir(docs);
     // });
 
     //Question 4:
-    restaurants.find({}).project({'_id': 0, 'restaurant_id': 1, 'name': 1, 'district': 1, 'address.zipcode': 1}).toArray(function (err, docs) {
+    // restaurants.find({}).project({"_id": 0, "restaurant_id": 1, "name": 1, "district": 1, "address.zipcode": 1}).toArray(function (err, docs) {
+    //     console.dir(docs);
+    // });
+
+    //Question 5:
+    // restaurants.find({"district": "Bronx"}).toArray(function (err, docs) {
+    //     console.dir(docs);
+    // });
+
+    //Question 6:
+    // restaurants.find({"district": "Bronx"}).limit(5).toArray(function (err, docs) {
+    //     console.dir(docs);
+    // });
+
+    //Question 7:
+    // restaurants.find({"district": "Bronx"}).skip(5).limit(5).toArray(function (err, docs) {
+    //     console.dir(docs);
+    // });
+
+    //Question 8:
+    // restaurants.find({"address.coord.0":{$lt:-95.754168}}).toArray(function (err, docs) {
+    //     require('http').createServer((req, res) => {
+    //         res.end(JSON.stringify(docs));
+    //     }).listen(3008, () => console.log('localhost:3008'));
+    // });
+
+    //Question 9:
+    // restaurants.find({"cuisine": {$ne: "American"}, "grades": {$elemMatch: {"score": {$gt: 70}}}, "address.coord.0": {$lt: -65.754168} }).toArray(function (err, docs) {
+    //         require('http').createServer((req, res) => {
+    //             res.end(JSON.stringify(docs));
+    //         }).listen(3008, () => console.log('localhost:3008'));
+    // })
+
+    //Question 10:
+    // restaurants.find({name: {$regex: "^Wil"}}).project({"restaurant_id":1, "name":1, "district":1, "cuisine":1, _id: 0}).toArray(function (err, docs) {
+    //         console.dir(docs);
+    // });
+
+    //Question 11:
+    // restaurants.find({name: {$regex: "ces$"}}).project({"restaurant_id":1, "name":1, "district":1, "cuisine":1, _id: 0}).toArray(function (err, docs) {
+    //         console.dir(docs);
+    // });
+
+    //Question 12:
+    // restaurants.find({name: {$regex: "Reg"}}).project({"restaurant_id":1, "name":1, "district":1, "cuisine":1, _id: 0}).toArray(function (err, docs) {
+    //     console.dir(docs);
+    // });
+
+    //Question 13:
+    restaurants.find({$and: [{"district": "Bronx"}, {"cuisine": {$in: ["American ", "Chinese"]} }]}).toArray(function (err, docs) {
         console.dir(docs);
-    });
+    })
+
+
 
 
 });
